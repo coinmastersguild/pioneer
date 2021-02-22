9dd4129ac58283542b46f41617f31cd6e08cc546
highlander <bithighlander@gmail.com>
2021-02-21 18:25:16 -0700

    begin popup

---
+++ apps/pioneer-bex/src/pages/Popup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>You are Broke!</h3>
+  </div>
+</template>
+
+<script lang="ts">
+  import ExampleComponent from 'components/OptionsComponent.vue';
+  import Vue from 'vue';
+
+  export default Vue.extend({
+    name: 'Popup',
+    components: { ExampleComponent },
+    data() {
+    }
+  });
+</script>
---
+++ docs/bex.md
@@ -0,0 +1,3 @@
+### links
+
+https://anobjectisa.medium.com/how-does-a-chrome-extension-work-web-development-6e85bd2bccc1
