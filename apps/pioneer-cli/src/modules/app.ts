32278c886f7726b2f741f181229239fa7a18a42e
highlander <bithighlander@gmail.com>
2021-04-04 17:54:54 -0500

    work on signing

--- apps/pioneer-cli/src/modules/app.ts
+++ apps/pioneer-cli/src/modules/app.ts
@@ -3,9 +3,8 @@
 
     App cycle
 
-
-
             -Highlander
+
  */
 const TAG = " | App | ";
 const log = require("loggerdog-client")();
@@ -19,6 +18,8 @@ const open = require("open");
 const vorpal = require("vorpal")();
 const bcrypt = require("bcryptjs");
 const prettyjson = require('prettyjson');
+let wait = require('wait-promise');
+let sleep = wait.sleep;
 
 import {
     prompt_fio_enable,
@@ -45,6 +46,13 @@ function standardRandomBytesFunc(size:any) {
     return CryptoJS.lib.WordArray.random(size).toString()
 }
 
+let apps = [
+    {
+        service:"AsgardDex (WEB)",
+        url:"https://swaps.pro",
+    }
+]
+
 export async function onStart() {
     let tag = TAG + " | onStart | ";
     try {
@@ -55,6 +63,9 @@ export async function onStart() {
         let info = await Hardware.info()
         info.type = 'keepkey'
         log.info(tag,"keepkey: info: ",info)
+
+        let keepkeyStatus = await App.hardwareState()
+        log.info("keepkeyStatus: ",keepkeyStatus)
 
         //get config
         let config = await App.getConfig()
@@ -68,203 +79,224 @@ export async function onStart() {
             config = await App.getConfig()
         }
 
-        if(config && !config.promptedFio){
-            //opt in to FIO
-            //if unkown state, request
-            if(!FIO_ACCEPT && !FIO_REJECT){
-                //prompt
-                let accepted = await prompt_fio_enable()
-                // @ts-ignore
-                if(accepted){
-                    App.updateConfig({FIO_ACCEPT:true});
-                    App.updateConfig({promptedFio:true});
-                    FIO_ACCEPT = true
-                } else {
-                    App.updateConfig({FIO_REJECT:true});
-                    App.updateConfig({promptedFio:true});
-                    FIO_REJECT = true
+        if(keepkeyStatus.state > 3) {
+            if(config && !config.promptedFio){
+                //opt in to FIO
+                //if unkown state, request
+                if(!FIO_ACCEPT && !FIO_REJECT){
+                    //prompt
+                    let accepted = await prompt_fio_enable()
+                    // @ts-ignore
+                    if(accepted){
+                        App.updateConfig({FIO_ACCEPT:true});
+                        App.updateConfig({promptedFio:true});
+                        FIO_ACCEPT = true
+                    } else {
+                        App.updateConfig({FIO_REJECT:true});
+                        App.updateConfig({promptedFio:true});
+                        FIO_REJECT = true
+                    }
                 }
+                config = await App.getConfig()
             }
-            config = await App.getConfig()
-        }
 
-        if(config && !config.promptedPasswordless){
-            //prompt
-            let accepted = await prompt_password_enable()
-            App.updateConfig({promptedPasswordless:true});
-            // @ts-ignore
-            if(!accepted){
+            if(config && !config.promptedPasswordless){
+                //prompt
+                let accepted = await prompt_password_enable()
+                App.updateConfig({promptedPasswordless:true});
+                // @ts-ignore
+                if(!accepted){
+                    PASSWORDLESS_ENABLE = true
+                }
+                config = await App.getConfig()
+            }
+
+            if(config && config.temp){
                 PASSWORDLESS_ENABLE = true
             }
-            config = await App.getConfig()
-        }
 
-        if(config && config.temp){
-            PASSWORDLESS_ENABLE = true
-        }
+            /*
+                Password setup
 
-        /*
-            Password setup
+                Config password
+                    //wallet passwords
+             */
 
-            Config password
-                //wallet passwords
-         */
+            let password
 
-        let password
+            //if passwordless generate
+            if(PASSWORDLESS_ENABLE && !config.temp && !config.passwordHash){
+                let passwords = generator.generateMultiple(1, {
+                    length: 10,
+                    uppercase: false
+                });
+                password = passwords[0]
+                App.updateConfig({temp:password});
+                config = await App.getConfig()
+            } else if(!PASSWORDLESS_ENABLE && !config.passwordHash){
+                //Create NEW password
+                let responses = await prompt_password_create()
+                log.debug(tag,"create password: ",responses)
+                //TODO
+                //verify password1 2 match
+                if(responses.password !== responses.password2){
+                    log.info("Invalid input! password dont match!")
+                    await onStart()
+                    return
+                }
+                //verify password not weak
 
-        //if passwordless generate
-        if(PASSWORDLESS_ENABLE && !config.temp && !config.passwordHash){
-            let passwords = generator.generateMultiple(1, {
-                length: 10,
-                uppercase: false
-            });
-            password = passwords[0]
-            App.updateConfig({temp:password});
-            config = await App.getConfig()
-        } else if(!PASSWORDLESS_ENABLE && !config.passwordHash){
-            //Create NEW password
-            let responses = await prompt_password_create()
-            log.debug(tag,"create password: ",responses)
-            //TODO
-            //verify password1 2 match
-            if(responses.password !== responses.password2){
-                log.info("Invalid input! password dont match!")
-                await onStart()
-                return
-            }
-            //verify password not weak
-
-            //if accept
-            const hash = bcrypt.hashSync(responses.password, 10);
-            await App.updateConfig({passwordHash:hash});
+                //if accept
+                const hash = bcrypt.hashSync(responses.password, 10);
+                await App.updateConfig({passwordHash:hash});
                 //hash and create
 
-            //if reject
+                //if reject
                 //try again
 
-            password = responses.password
-        } else if(config.temp){
-            password = config.temp
-            //
+                password = responses.password
+            } else if(config.temp){
+                password = config.temp
+                //
 
-        } else if(config.passwordHash){
-            let passwordInput = await prompt_password_input()
-            log.debug(tag,"passwordInput: ",passwordInput)
-            //TODO hash & verify
-            let isValid = bcrypt.compareSync(passwordInput, config.passwordHash); // true
-            if(!isValid) {
-                log.info("Invalid password!")
-                await onStart()
-                return
+            } else if(config.passwordHash){
+                let passwordInput = await prompt_password_input()
+                log.debug(tag,"passwordInput: ",passwordInput)
+                //TODO hash & verify
+                let isValid = bcrypt.compareSync(passwordInput, config.passwordHash); // true
+                if(!isValid) {
+                    log.info("Invalid password!")
+                    await onStart()
+                    return
+                }
+
+                password = passwordInput
             }
 
-            password = passwordInput
-        }
+            let wallets = await App.getWalletNames()
+            log.debug(tag,"wallets: ",wallets)
 
-        let wallets = await App.getWalletNames()
-        log.debug(tag,"wallets: ",wallets)
+            //if fio get fio username
+            let username
+            if(config && !config.username){
+                //generate random
+                let randomChars = generator.generateMultiple(1, {
+                    length: 10,
+                    uppercase: false
+                });
+                username = "temp:"+randomChars[0]
+                config.username = username
+            }
 
-        //if fio get fio username
-        let username
-        if(config && !config.username){
-            //generate random
-            let randomChars = generator.generateMultiple(1, {
-                length: 10,
-                uppercase: false
-            });
-            username = "temp:"+randomChars[0]
-            config.username = username
-        }
+            //if no wallets
+            if(wallets.length > 0){
+                //wallet found
+                log.debug(tag,"Wallets found! ",wallets)
 
-        //if no wallets
-        if(wallets.length > 0){
-            //wallet found
-            log.debug(tag,"Wallets found! ",wallets)
+            } else {
+                //create wallet
+                let type:any = await prompt_create_wallet()
+                log.debug(tag,"type: ",type)
 
-        } else {
-            //create wallet
-            let type:any = await prompt_create_wallet()
-            log.debug(tag,"type: ",type)
+                // @ts-ignore
+                if(type === "create a new wallet") {
+                    log.debug(tag,"create new wallet!")
+                    // //create a new
+                    let randomBytesFunc = standardRandomBytesFunc
+                    const randomBytes = Buffer.from(randomBytesFunc(32), `hex`)
+                    if (randomBytes.length !== 32) throw Error(`Entropy has incorrect length`)
+                    let mnemonic = bip39.entropyToMnemonic(randomBytes.toString(`hex`))
 
-            // @ts-ignore
-            if(type === "create a new wallet") {
-                log.debug(tag,"create new wallet!")
-                // //create a new
-                let randomBytesFunc = standardRandomBytesFunc
-                const randomBytes = Buffer.from(randomBytesFunc(32), `hex`)
-                if (randomBytes.length !== 32) throw Error(`Entropy has incorrect length`)
-                let mnemonic = bip39.entropyToMnemonic(randomBytes.toString(`hex`))
-
-                //create
-                let wallet:any = {
-                    mnemonic,
-                    username:config.username,
-                    password
-                }
-                if(config.temp) wallet.temp = config.temp
-                //create wallet files
-                log.debug("1 creating wallet: ",wallet)
-                let resultCreate = await App.createWallet('software',wallet)
-                log.debug("result creating wallet: ",resultCreate)
-            } else if (type === "restore from seed") {
-                let seed = await prompt_seed_input()
-                //TODO validate seed
+                    //create
+                    let wallet:any = {
+                        mnemonic,
+                        username:config.username,
+                        password
+                    }
+                    if(config.temp) wallet.temp = config.temp
+                    //create wallet files
+                    log.debug("1 creating wallet: ",wallet)
+                    let resultCreate = await App.createWallet('software',wallet)
+                    log.debug("result creating wallet: ",resultCreate)
+                } else if (type === "restore from seed") {
+                    let seed = await prompt_seed_input()
+                    //TODO validate seed
                     //if invalid, ask for again
-                //create
-                let wallet:any = {
-                    mnemonic:seed,
-                    username:config.username,
-                    password
-                }
-                if(config.temp) wallet.temp = config.temp
-                //create wallet files
-                log.debug("2 creating wallet: ",wallet)
-                let resultCreate = await App.createWallet('software',wallet)
-                log.debug("result creating wallet: ",resultCreate)
-            } else if (type === "pair hardware wallet") {
-                log.info("pair hardware wallet!")
+                    //create
+                    let wallet:any = {
+                        mnemonic:seed,
+                        username:config.username,
+                        password
+                    }
+                    if(config.temp) wallet.temp = config.temp
+                    //create wallet files
+                    log.debug("2 creating wallet: ",wallet)
+                    let resultCreate = await App.createWallet('software',wallet)
+                    log.debug("result creating wallet: ",resultCreate)
+                } else if (type === "pair hardware wallet") {
+                    log.info("pair hardware wallet!")
 
-                if(info.features){
-                    //is locked?
-                    let pubkeysKeepkey = await Hardware.getPubkeys()
+                    if(info.features){
+                        //is locked?
+                        let pubkeysKeepkey = await Hardware.getPubkeys()
+
+                        //
+                        let resultPair = await App.pairKeepkey(info.features.deviceId,pubkeysKeepkey)
+                        log.info("resultPair: ",resultPair)
+                    } else {
+                        //try to reconnect
+                        log.error(tag,"Device Connected to other host!")
+                    }
 
                     //
-                    let resultPair = await App.pairKeepkey(info.features.deviceId,pubkeysKeepkey)
-                    log.info("resultPair: ",resultPair)
-                } else {
-                    //try to reconnect
-                    log.error(tag,"Device Connected to other host!")
                 }
-
-                //
             }
+
+
+            //start app
+            log.debug("pre-init CONFIG: ",config)
+            if(password) config.password = password
+            if(!config.temp && !config.password) throw Error("102: password required for startup! ")
+
+            //hardware
+            config.hardware = true
+
+            let keepkeyStatus2 = await App.hardwareState()
+            log.info(tag,"keepkeyStatus2: ",keepkeyStatus2)
+
+            let isTestnet = true
+            let resultInit = await App.init(config,isTestnet)
+            log.debug("resultInit: ",resultInit)
+
+            //if username is temp
+            if(config.FIO_ACCEPT && config.username.indexOf("temp:") >= 0){
+                //FIO name enable
+                let fioPubkey = await App.getFioPubkey()
+                log.debug(tag,"fioPubkey: ",fioPubkey)
+
+                let usernames = await App.getFioAccountsByPubkey(fioPubkey)
+                if(usernames.length === 0){
+                    //open fio signup
+                    open("https://reg.fioprotocol.io/ref/shapeshift?publicKey=" + fioPubkey);
+                } else {
+                    username = usernames.fio_addresses[0].fio_address
+                    if(!username) throw Error("failed to find fio username!")
+                    App.updateConfig({username});
+                }
+            }
+            return resultInit
+        }else if(keepkeyStatus.state === 2){
+            //prompt pin
+            console.log("Device Locked!")
+            Hardware.displayPin()
+            //TODO unlock pin
+            onStart()
+        } else {
+
+            await sleep(2000)
+            onStart()
         }
 
-
-        //start app
-        log.debug("pre-init CONFIG: ",config)
-        if(password) config.password = password
-        if(!config.temp && !config.password) throw Error("102: password required for startup! ")
-
-        let resultInit = await App.init(config)
-        log.debug("resultInit: ",resultInit)
-
-        //if username is temp
-        if(config.FIO_ACCEPT && config.username.indexOf("temp:") >= 0){
-            //FIO name enable
-            let fioPubkey = await App.getFioPubkey()
-            log.debug(tag,"fioPubkey: ",fioPubkey)
-
-            let usernames = await App.getFioAccountsByPubkey(fioPubkey)
-            if(usernames.length === 0){
-                //open fio signup
-                open("https://reg.fioprotocol.io/ref/shapeshift?publicKey=" + fioPubkey);
-            } else {
-                username = usernames.fio_addresses[0].fio_address
-                if(!username) throw Error("failed to find fio username!")
-                App.updateConfig({username});
-            }
-        }
 
 
 
@@ -273,7 +305,6 @@ export async function onStart() {
             //register config
             //register wallet
 
-        return resultInit
     } catch (e) {
         console.error(tag, "e: ", e);
         return {};
@@ -389,7 +420,9 @@ export async function onRun() {
         }
         log.debug("context: ",context)
         const map = describe.map(contextView);
-
+        App.openApp = function(url:string){
+            open("https://swaps.pro")
+        }
         //
         let appView:any = {}
         let appFunctions = Object.keys(App)
@@ -412,10 +445,19 @@ export async function onRun() {
             info: "",
         };
 
+        // let openApp = function(url:string){
+        //     open("https://swaps.pro")
+        // }
+        //
+        // let mockModule = {
+        //     openApp:openApp('https://swaps.pro')
+        // }
+
         //               view        controller
         await loadModule(contextView,context)
         await loadModule(Hardware,Hardware)
         await loadModule(appView,App)
+        // await loadModule(mockModule,mockModule)
 
         vorpal
             .delimiter(prompt)
