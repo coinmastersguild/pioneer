18b5412c646ac9de747ccd447be6852660fde4a5
highlander <bithighlander@gmail.com>
2021-05-16 16:24:08 -0600

    version bump

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ set -e
 git config user.email "circlciBot@dontemailme.com"
 git config user.name "cibot"
 
+git add -A && git commit -m "version bump"
 #yarn add lerna -g --ignore-workspace-root-check
 lerna version minor
 
