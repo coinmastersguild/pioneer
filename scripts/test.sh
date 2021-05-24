b2f766af5e88acf70b6c5f7f37e72c34b943b3c9
cibot <circlciBot@dontemailme.com>
2021-05-23 23:53:18 -0600

    force debug

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,7 +9,8 @@ if [[ $2 = 'true' ]]
 then
   cd e2e/sdk-swaps/thorchain-e2e-swap && npm run run-dev
 else
-  cd e2e/sdk-swaps/thorchain-e2e-swap && npm run test
+  cd e2e/sdk-swaps/thorchain-e2e-swap && npm run run-dev
+  #cd e2e/sdk-swaps/thorchain-e2e-swap && npm run test
 fi
 
 
