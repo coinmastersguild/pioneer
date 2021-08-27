3e78efa605c60000cef4c3cab1569b7067037ed9
highlander <bithighlander@gmail.com>
2021-08-27 15:28:11 -0600

    debug test

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,12 +11,13 @@ coin=$3
 # DEV move (dont use jest)
 if [[ $2 = 'true' ]]
 then
-  cd e2e/sdk-transfers/thorchain-e2e-transfer && npm run dev && cd ../../../ &&/
-  cd e2e/sdk-transfers/ethereum-e2e-transfer && npm run dev && cd ../../../ &&/
-#   cd e2e/sdk-support/rbf-ethereum && npm run dev && cd ../../../ &&/
-  cd e2e/sdk-support/abort-tx && npm run dev && cd ../../../ &&/
-  cd e2e/sdk-support/forget-user && npm run dev && cd ../../../ &&/
-  cd e2e/sdk-support/querykey-migration && npm run dev && cd ../../../ &&/
+  cd e2e/sdk-support/abort-tx && npm run dev && cd ../../../
+#  cd e2e/sdk-transfers/thorchain-e2e-transfer && npm run dev && cd ../../../ &&/
+#  cd e2e/sdk-transfers/ethereum-e2e-transfer && npm run dev && cd ../../../ &&/
+##   cd e2e/sdk-support/rbf-ethereum && npm run dev && cd ../../../ &&/
+#  cd e2e/sdk-support/abort-tx && npm run dev && cd ../../../ &&/
+#  cd e2e/sdk-support/forget-user && npm run dev && cd ../../../ &&/
+#  cd e2e/sdk-support/querykey-migration && npm run dev && cd ../../../ &&/
 #  cd e2e/sdk-support/context-switching && npm run dev && cd ../../../ &&/
 #  cd e2e/sdk-swaps/bitcoincash-e2e-swap && npm run dev && cd ../../../ &&/
 #  cd e2e/sdk-swaps/etherum-e2e-swap && npm run dev && cd ../../../ &&/
@@ -31,14 +32,16 @@ else
 #  cd e2e/sdk-support/forget-user && npm run test && cd ../../../ &&/
 #  cd e2e/sdk-support/querykey-migration && npm run test && cd ../../../ &&/
 #  cd e2e/sdk-support/rbf-ethereum && npm run test && cd ../../../ &&/
-  #transfers
-  cd e2e/sdk-transfers/cosmos-e2e-transfer && npm run test && cd ../../../ &&/
-  #ibc depsoit
-  cd e2e/sdk-tendermint-custom/cosmos-e2e-ibc-deposit && npm run test && cd ../../../ &&/
-  #osmosis
-  cd e2e/sdk-tendermint-custom/osmosis-e2e-swap && npm run test && cd ../../../ &&/
-  cd e2e/sdk-tendermint-custom/osmosis-e2e-delegate && npm run test && cd ../../../ &&/
-  cd e2e/sdk-tendermint-custom/osmosis-e2e-lp-add && npm run test && cd ../../../
+
+#  #transfers
+#  cd e2e/sdk-transfers/cosmos-e2e-transfer && npm run test && cd ../../../ &&/
+#  #ibc depsoit
+#  cd e2e/sdk-tendermint-custom/cosmos-e2e-ibc-deposit && npm run test && cd ../../../ &&/
+#  #osmosis
+#  cd e2e/sdk-tendermint-custom/osmosis-e2e-swap && npm run test && cd ../../../ &&/
+#  cd e2e/sdk-tendermint-custom/osmosis-e2e-delegate && npm run test && cd ../../../ &&/
+#  cd e2e/sdk-tendermint-custom/osmosis-e2e-lp-add && npm run test && cd ../../../
+
 #remember last entry can NOT have &&/
 
 #thorchain offline TODO re-enable
