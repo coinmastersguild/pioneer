2e0296b2021f6c1d975ed1e4ee1782a6bfeac8bd
highlander <bithighlander@gmail.com>
2021-09-24 00:09:26 -0600

    terra address

--- modules/coins/terra/terra-crypto/__tests__/test-module.js
+++ modules/coins/terra/terra-crypto/__tests__/test-module.js
@@ -12,13 +12,16 @@ let crypto = require("../lib/index")
 
 let test_seed = "alcohol woman abuse must during monitor noble actual mixed trade anger aisle"
 
-//let test_seed = process.env['WALLET_MAIN']
+// let test_seed = process.env['OSMOS_WALLET_1']
 // console.log("test_seed: ",test_seed)
 
 crypto.generateWalletFromSeed(test_seed)
     .then(function(resp){
         console.log("resp: ",resp)
-
+        let address = "terra1ls33ayg26kmltw7jjy55p32ghjna09zp7kgw2a"
+        if(resp.masterAddress === 'terra1ls33ayg26kmltw7jjy55p32ghjna09zp7kgw2a'){
+            console.log('winning')
+        }
         //assert
     })
 
