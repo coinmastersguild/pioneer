552a11f07c015bffbeb66fac4d10eefc42136de9
bithighlander <bithighlander@gmail.com>
2024-01-02 21:29:43 -0300

    bump

--- scripts/npm-update-modules.sh
+++ scripts/npm-update-modules.sh
@@ -1,1 +1,13 @@
-for dir in modules/*; do $(cd "$dir" && npm update) &; done &
+#!/bin/bash
+
+# Loop through each directory in modules/*
+for dir in modules/*; do
+  if [ -d "$dir" ]; then
+    echo "Updating @pioneer-platform packages in $dir"
+    # Change to the directory and update @pioneer-platform packages
+    (cd "$dir" && yarn upgrade --scope @pioneer-platform) &
+  fi
+done
+
+# Wait for all background processes to finish
+wait
