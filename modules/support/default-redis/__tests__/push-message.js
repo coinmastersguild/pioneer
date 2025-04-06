dfd31b79e062d70483087b773d9ff367e410b95c
highlander <bithighlander@gmail.com>
2025-04-05 22:45:38 -0300

    debug testing

--- modules/support/default-redis/__tests__/push-message.js
+++ modules/support/default-redis/__tests__/push-message.js
@@ -5,4 +5,4 @@ let message = {
     queryKey:'adsfgdfgds3sdfsd'
 }
 
-publisher.publish('clubmoon-events','Fuck you Joose, you are a fucking faggot. ill rape your bitch ass mouth')
+publisher.publish('keepkey-support',JSON.stringify(message))
